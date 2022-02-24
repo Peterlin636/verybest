0963222d838ba45ae61c280c564f0189e70c4aad
anonymous anonymous <peter.lin@kellogg.northwestern.edu>
2022-02-24 14:53:32 +0000

    Remove controller comments

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,29 +1,24 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: %i[show edit update destroy]
 
-  # GET /restaurants
   def index
     @q = Restaurant.ransack(params[:q])
     @restaurants = @q.result(distinct: true).includes(:likes, :bookmarks,
                                                       :dishes).page(params[:page]).per(10)
   end
 
-  # GET /restaurants/1
   def show
     @dish = Dish.new
     @bookmark = Bookmark.new
     @like = Like.new
   end
 
-  # GET /restaurants/new
   def new
     @restaurant = Restaurant.new
   end
 
-  # GET /restaurants/1/edit
   def edit; end
 
-  # POST /restaurants
   def create
     @restaurant = Restaurant.new(restaurant_params)
 
@@ -34,7 +29,6 @@ class RestaurantsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /restaurants/1
   def update
     if @restaurant.update(restaurant_params)
       redirect_to @restaurant, notice: "Restaurant was successfully updated."
@@ -43,7 +37,6 @@ class RestaurantsController < ApplicationController
     end
   end
 
-  # DELETE /restaurants/1
   def destroy
     @restaurant.destroy
     redirect_to restaurants_url,
@@ -52,12 +45,10 @@ class RestaurantsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def restaurant_params
     params.require(:restaurant).permit(:dish_id)
   end
