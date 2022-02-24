77d7d00c0e3362495f6b6aec7f0bda4923f1d200
anonymous anonymous <peter.lin@kellogg.northwestern.edu>
2022-02-24 14:51:11 +0000

    Direct association between restaurants and bookmarks

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :bookmarks,
+             :dependent => :destroy
+
   has_many   :likes,
              :dependent => :destroy
 
