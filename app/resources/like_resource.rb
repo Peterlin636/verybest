40cfed18e48bd2820b210c727aabd587146708ce
anonymous anonymous <peter.lin@kellogg.northwestern.edu>
2022-02-24 14:52:39 +0000

    Generate graphiti resources for likes

---
+++ app/resources/like_resource.rb
@@ -0,0 +1,13 @@
+class LikeResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :dish_id, :integer
+  attribute :restaurant_id, :integer
+  attribute :user_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
